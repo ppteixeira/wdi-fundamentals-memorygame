7114657a46c7ec6758b6382a56bba47cccac5ab8
PEDRO ARAUJO <ppteixeira@outlook.com>
2016-09-08 20:05:40 +1000

    Added JS Comparison and alert

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,10 @@ var cardOne = "king";
 var cardTwo = "queen";
 var cardThree = "queen";
 var cardFour = "king";
+
+if (cardOne == cardTwo) {
+  alert("You found a match!");
+
+}else {
+  alert("Sorry, try again");
+}
